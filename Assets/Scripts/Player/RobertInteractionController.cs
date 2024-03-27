1a694ff67e078d4ad618afdb254b643f208e2b24
MayaHill <71844243+MayaHill@users.noreply.github.com>
2024-03-27 18:02:28 +0900

    Robert player can now interact with objects carrying a script that inherits from InteractableController script. Added a door which the player can open / close An outline and prompt message appears when an object can be interacted with

---
+++ Assets/Scripts/Player/RobertInteractionController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class RobertInteractionController : MonoBehaviour
+{
+    [Header("Interaction")]
+    [SerializeField] private float interactionDistance = 3f;
+    [SerializeField] private LayerMask interactableLayer;
+
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI promptText;
+
+    InteractableController _currentInteractable;
+
+    private void Awake()
+    {
+        ResetCurrentInteractable();
+    }
+
+    private void OnEnable()
+    {
+        InputManager.Instance.InputControls.Game.Interact.started += OnInteract;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.InputControls.Game.Interact.started -= OnInteract;
+    }
+
+    private void Update()
+    {
+        CheckInteractable();
+    }
+
+    private void CheckInteractable()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, interactionDistance, interactableLayer)) {
+            InteractableController interactable = hit.collider.GetComponent<InteractableController>();
+            if (interactable)
+            {
+                if (_currentInteractable != interactable)
+                {
+                    ResetCurrentInteractable();
+                }
+                _currentInteractable = interactable;
+                _currentInteractable.ToggleOutline(true);
+                promptText.text = "Click to " + _currentInteractable.GetPromptMessage();
+                return;
+            }
+        }
+
+        ResetCurrentInteractable();
+    }
+
+    private void ResetCurrentInteractable()
+    {
+        promptText.text = string.Empty;
+
+        if (_currentInteractable)
+        {
+            _currentInteractable.ToggleOutline(false);
+            _currentInteractable = null;
+        }
+    }
+
+    private void OnInteract(InputAction.CallbackContext ctx)
+    {
+        if (!_currentInteractable)
+        {
+            return;
+        }
+
+        _currentInteractable.Interact();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(Camera.main.transform.position, Camera.main.transform.position + Camera.main.transform.forward * interactionDistance);
+    }
+}
