ad65ce830602c83a4397ed35d95816d8038a10b7
DamienDemontis <damien.demontis@epitech.eu>
2024-05-07 23:52:47 +0900

    [FIX] lobby folder rename

---
+++ Assets/Scripts/Player/PlayerSetup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Mirror;
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+    
+    private void Start()
+    {
+        Debug.Log("isLocalPlayer = " + isLocalPlayer);
+        if(!isLocalPlayer) {
+            for (int i = 0; i < componentsToDisable.Length; i++) {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+
+    }
+}
