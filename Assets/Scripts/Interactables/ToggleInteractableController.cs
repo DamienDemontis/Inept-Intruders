849d173b557ef4a549ded1c3fe326ddd800162b6
MayaHill <71844243+MayaHill@users.noreply.github.com>
2024-05-07 21:30:22 +0900

    Added pressure plate object that activates when the player drops a box on it, and deactivated when the box is removed

---
+++ Assets/Scripts/Interactables/ToggleInteractableController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleInteractableController : MonoBehaviour, IInteractable
+{
+    public virtual void Interact()
+    {
+        Debug.Log("Toggle interacted with " + name);
+    }
+
+    public void CancelInteract()
+    {
+        Debug.Log("Toggle cancel interacted with " + name);
+    }
+
+    public string GetId()
+    {
+        return string.Empty;
+    }
+}
