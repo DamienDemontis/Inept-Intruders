9becb1925cd3aae5179af5ef2be093910bb3791f
Louis <louis.obeniche-rousse@epitech.eu>
2024-06-05 10:54:03 +0900

    add: zoom in and out of monitor

--- Assets/Scripts/Player/CamGuy/CamGuyCamera.cs
+++ Assets/Scripts/Player/CamGuy/CamGuyCamera.cs
@@ -25,17 +25,11 @@ public class CamGuyCamera : MonoBehaviour
 
     private Transform _target;
     private float     _distanceFromTarget;
-    private Vector3   _positionFromCamGuy;
-
-    private Vector3    _baseLocalPosition;
-    private Vector3    _basePosition;
-    private Quaternion _baseRotation;
+    private Transform _baseTransfomer;
 
     private void Start()
     {
-        _baseLocalPosition = transform.localPosition;
-        _basePosition = _camGuy.transform.position;
-        _baseRotation = _camGuy.transform.rotation;
+        _baseTransfomer = _camGuy.transform;
     }
 
     private void Update()
@@ -45,37 +39,6 @@ public class CamGuyCamera : MonoBehaviour
             Debug.LogWarning("[CamGuyCamera::Update] cam guy null");
             return;
         }
-
-        //_basePosition = _camGuy.transform.position + _baseLocalPosition;
-        //_baseRotation = _camGuy.transform.rotation;
-
-        //UpdateCameraRotation();
-    }
-
-    private void Awake()
-    {
-        //Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
-    }
-
-    private void UpdateCameraRotation()
-    {
-        Vector2 mouseDelta_ = InputManager.Instance.GetMouseDelta();
-        Vector2 mouseDelta = mouseDelta_ * sensitivity * Time.deltaTime;
-        _cameraRotation += new Vector2(-mouseDelta.y, mouseDelta.x);
-
-        if (clampXRotation)
-        {
-            _cameraRotation.x = Mathf.Clamp(_cameraRotation.x, minRotation.x, maxRotation.x);
-        }
-        if (clampYRotation)
-        {
-            _cameraRotation.y = Mathf.Clamp(_cameraRotation.y, minRotation.y, maxRotation.y);
-        }
-
-        Quaternion targetRotation = Quaternion.Euler(_cameraRotation);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, maxRotationSpeed * Time.deltaTime);
-        orientation.rotation = Quaternion.Euler(0, _cameraRotation.y, 0);
     }
 
     public void StartTransition(Transform target, float distanceFromTarget, float transitionDuration)
@@ -89,48 +52,20 @@ public class CamGuyCamera : MonoBehaviour
 
     public void ResetToBase(float transitionDuration = 2.5f)
     {
-        StartCoroutine(ResetTransition());
+        StartTransition(_baseTransfomer, 0.0f, transitionDuration);
     }
 
     IEnumerator Transition()
     {
         float t = 0.0f;
         Vector3 startingPos    = transform.position;
-        //Quaternion startingRot = transform.rotation;
+        Quaternion startingRot = transform.rotation;
 
         Vector3 directionToTarget = _target.position - transform.position;
         directionToTarget.Normalize();
         Vector3 modifiedTargetPos = _target.position - directionToTarget * _distanceFromTarget;
 
-        //Quaternion targetRotation = _target.rotation;
-
-        while (t < 1.0f)
-        {
-            t += Time.deltaTime * (Time.timeScale / _transitionDuration);
-
-            float curveT = _transitionCurve.Evaluate(t);
-
-            transform.position = Vector3.Lerp(startingPos, modifiedTargetPos, curveT);
-            //transform.rotation = Quaternion.Slerp(startingRot, targetRotation, curveT);
-
-            yield return null;
-        }
-
-        transform.position = modifiedTargetPos;
-        //transform.rotation = targetRotation;
-    }
-
-    IEnumerator ResetTransition()
-    {
-        float t = 0.0f;
-        Vector3 startingPos = transform.position;
-        Quaternion startingRot = transform.rotation;
-
-        Vector3 directionToTarget = _basePosition - transform.position;
-        directionToTarget.Normalize();
-        Vector3 modifiedTargetPos = _basePosition - directionToTarget;
-
-        Quaternion targetRotation = _baseRotation;
+        Quaternion targetRotation = _target.rotation;
 
         while (t < 1.0f)
         {
@@ -147,5 +82,4 @@ public class CamGuyCamera : MonoBehaviour
         transform.position = modifiedTargetPos;
         transform.rotation = targetRotation;
     }
-
 }
