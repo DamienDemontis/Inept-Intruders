1459a515bf00862f0bc5abb1bceda878c8dd2742
Louis <louis.obeniche-rousse@epitech.eu>
2024-06-17 03:28:09 +0900

    add: music

--- Assets/Scripts/Scripts_lobby/UI/CharacterSelect/CharacterSelectButton.cs
+++ Assets/Scripts/Scripts_lobby/UI/CharacterSelect/CharacterSelectButton.cs
@@ -6,6 +6,8 @@ public class CharacterSelectButton : MonoBehaviour
     [SerializeField] private Image iconImage;
     [SerializeField] private GameObject disabledOverlay;
     [SerializeField] private UnityEngine.UI.Button button;
+    [SerializeField] private AudioSource audioSourceRobert;
+    [SerializeField] private AudioSource audioSourceCamGuy;
 
     private CharacterSelectDisplay characterSelect;
 
@@ -24,6 +26,25 @@ public class CharacterSelectButton : MonoBehaviour
     public void SelectCharacter()
     {
         characterSelect.Select(Character);
+
+        if (audioSourceCamGuy == null || audioSourceRobert == null)
+        {
+            Debug.LogWarning($"[CharacterSelectButton::SelectCharacter] audio source is null.");
+            return;
+        }
+
+        Debug.Log($"[CharacterSelection] id : {Character.Id}");
+
+        if (Character.Id == 1)
+        {
+            audioSourceRobert.Play();
+            audioSourceCamGuy.Pause();
+        }
+        if (Character.Id == 2)
+        {
+            audioSourceCamGuy.Play();
+            audioSourceRobert.Pause();
+        }
     }
 
     public void SetDisabled()
@@ -31,5 +52,17 @@ public class CharacterSelectButton : MonoBehaviour
         IsDisabled = true;
         disabledOverlay.SetActive(true);
         button.interactable = false;
+    }
+
+    public AudioSource CamGuyOST
+    {
+        get { return audioSourceCamGuy; }
+        set { audioSourceCamGuy = value; }
+    }
+
+    public AudioSource RobertOST
+    {
+        get { return audioSourceRobert; }
+        set { audioSourceRobert = value; }
     }
 }
